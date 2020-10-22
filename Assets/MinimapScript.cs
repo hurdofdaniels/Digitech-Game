d5fe39cb34b75bdf89b2782bef4cb33d4d13d3e2
Daniel Hurd <hurdofdaniels@gmail.com>
2020-10-23 07:26:31 +1300

    Inital commit

---
+++ Assets/MinimapScript.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapScript : MonoBehaviour
+{
+
+    public float panSpeed = 20f;
+
+    public Vector2 panLimit;
+    
+    public void Update()
+    {
+        
+        Vector3 pos = transform.position;
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
+
+        transform.position = pos;
+    }
+
+    /*public Transform player;
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }*/
+}
