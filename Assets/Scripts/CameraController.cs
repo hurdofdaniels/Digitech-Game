62794af71b6b94d3353b048ac0cfab4c99437c04
Daniel Hurd <hurdofdaniels@gmail.com>
2020-11-02 20:40:20 +1300

    Changing camera movement - once again

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,21 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
 
-    public Transform player;
+    public Transform playerPos;
     
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position;
+        transform.position = playerPos.position;
     }
 }
