5cc0b32aa74768d402beaae6f7154b86e4cf5b4b
Daniel Hurd <hurdofdaniels@gmail.com>
2020-11-03 23:00:22 +0000

    Fixing camera movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
+    public Transform cameraPos;
     public Transform playerPos;
     public float lerpSpeed = 0.1f;
+    public float rotationSpeed = 20.0f;
 
     void Start()
     {
@@ -16,16 +18,39 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        transform.position = playerPos.position;
+        /*
+         * transform.position = playerPos.position;
+         */
     }
 
     void FixedUpdate()
     {
         //transform.rotation = playerPos.rotation;
 
-        Quaternion rotationFrom = transform.rotation;
-        Quaternion rotationTo = playerPos.rotation;
+        /*
+         * Quaternion rotationFrom = transform.rotation;
+         * Quaternion rotationTo = playerPos.rotation;
+         * transform.rotation = Quaternion.Lerp(rotationFrom, rotationTo, lerpSpeed);
+         */
+    }
 
-        transform.rotation = Quaternion.Lerp(rotationFrom, rotationTo, lerpSpeed);
+    void LateUpdate()
+    {
+        if (transform.position != playerPos.position)
+            transform.position = playerPos.position;
+            Quaternion rotationFrom = transform.rotation;
+            Quaternion rotationTo = playerPos.rotation;
+            transform.rotation = Quaternion.Lerp(rotationFrom, rotationTo, lerpSpeed);
+        
+        
+        if (Input.GetKey(KeyCode.Q))
+            cameraPos.RotateAround(playerPos.position, 
+                                    Vector3.up, 
+                                    rotationSpeed * Time.deltaTime);
+        
+        if (Input.GetKey(KeyCode.E))
+            cameraPos.RotateAround(playerPos.position, 
+                                    -Vector3.up, 
+                                    rotationSpeed * Time.deltaTime);
     }
 }
