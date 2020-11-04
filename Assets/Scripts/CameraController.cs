b2b306daa3a6b1d2e48b76f4d4a94f0b0c8d37b8
Daniel Hurd <hurdofdaniels@gmail.com>
2020-11-04 01:23:14 +0000

    Cant remember the changes i made

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -36,21 +36,41 @@ public class CameraController : MonoBehaviour
 
     void LateUpdate()
     {
+        cameraLerp();
+        cameraRotation();
+    }
+
+    private void cameraLerp()
+    {
         if (transform.position != playerPos.position)
+        {
             transform.position = playerPos.position;
             Quaternion rotationFrom = transform.rotation;
             Quaternion rotationTo = playerPos.rotation;
             transform.rotation = Quaternion.Lerp(rotationFrom, rotationTo, lerpSpeed);
-        
-        
+            lerpToOriginalPosition();
+        }
+    }
+
+    private void cameraRotation()
+    {
         if (Input.GetKey(KeyCode.Q))
             cameraPos.RotateAround(playerPos.position, 
-                                    Vector3.up, 
-                                    rotationSpeed * Time.deltaTime);
+                Vector3.up, 
+                rotationSpeed * Time.deltaTime);
         
         if (Input.GetKey(KeyCode.E))
             cameraPos.RotateAround(playerPos.position, 
-                                    -Vector3.up, 
-                                    rotationSpeed * Time.deltaTime);
+                -Vector3.up, 
+                rotationSpeed * Time.deltaTime);
+    }
+
+    private void lerpToOriginalPosition()
+    {
+        // LookRotate back to o.g. pos
+        Quaternion rotationFrom = cameraPos.rotation;
+        Quaternion rotationTo = Quaternion.Euler(new Vector3(27.107f, -2.311f, 0.9030001f));
+        cameraPos.rotation = Quaternion.Lerp(rotationFrom, rotationTo, lerpSpeed);
+         
     }
 }
